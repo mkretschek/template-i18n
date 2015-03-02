0c35618090a2e2980451d57339090dcd0a3dc8cc
Mathias Kretschek <mathias@kretschek.com.br>
2015-03-02 14:47:05 -0300

    feat(test): add --reporter option to test task

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,16 @@
 
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var yargs = require('yargs');
 
 var chai = require('chai');
 var sinonChai = require('sinon-chai');
 chai.use(sinonChai);
 
+var argv = yargs.argv;
+
 gulp.task('test', function () {
+  var reporter = argv.reporter || 'dot';
   return gulp.src('lib/**/*.test.js', {read: false})
-    .pipe(mocha({reporter: 'dot'}));
+    .pipe(mocha({reporter: reporter}));
 });
